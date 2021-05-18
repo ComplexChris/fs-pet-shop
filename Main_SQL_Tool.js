cad754d20e7d7b51768faf9c662004034d05a00d
ComplexChris <ChrisNguyen180@gmail.com>
2021-05-18 13:27:27 -0500

    Adding project before PG implementation.

--- Main_SQL_Tool.js
+++ Main_SQL_Tool.js
@@ -13,26 +13,44 @@ class Querys{
         });;
     } // Constructor Closer
 
-    MakeQuery(command){
+    MakeQuery(command, callBack){
         // Makes a query with the specified command
         this.pool.query( command , (err, res) => {
             console.log(res);
+            if(callBacK){
+                callBack(res)
+            }
+            else{
+                return(err)
+            }
         });
     } // MakeQuery Closer
 
-    ClosePool(){
-        pool.end();
+    ClosePool(timeOut){
+        timeOut = timeOut || 10000;
+        const [main, warn] = [timeOut*.95, timeOut*.05]
+        setTimeout( ()=>{
+            console.log(`\n!!! Pool is closing in: ${ warn/60000 } minutes !!!\n`,  "\x1b[31m")
+            setTimeout( ()=>{
+                pool.end()
+                console.log("\n!!! Pool is now closed !!!\n", "\x1b[31m") 
+            },warn )
+        }, main );
+    }
+
+    CreateDB(db_name){
+        exec(`createdb ${db_name}`, (error, stdout, stderr) => {
+            if (stderr) {
+                console.log(`stderr: ${stderr}`);
+                return;
+            }
+            console.log(`stdout: ${stdout}`);
+        });
     }
 
 }
 
-const test = new Querys( {password: 'password123'} );
-
-test.MakeQuery("SELECT NOW()");
-
 
 module.exports = {
     Querys
-}
-
-setTimeout( {console.log("Test")}, 1.8e+6 )
+};
